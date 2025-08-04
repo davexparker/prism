3a188505667a6f06afabd481ef02d45db6535876
Dave Parker <dave.x.parker@gmail.com>
2025-08-04 21:21:07 +0100

    Full LTL for IDTMCs.

--- prism/src/explicit/IDTMCModelChecker.java
+++ prism/src/explicit/IDTMCModelChecker.java
@@ -30,6 +30,7 @@ import java.util.BitSet;
 import java.util.PrimitiveIterator;
 
 import acceptance.AcceptanceReach;
+import acceptance.AcceptanceType;
 import common.IntSet;
 import common.IterableStateSet;
 import explicit.rewards.MCRewards;
@@ -59,7 +60,52 @@ public class IDTMCModelChecker extends ProbModelChecker
 	}
 
 	// Model checking functions
-	
+
+	@Override
+	protected StateValues checkProbPathFormulaLTL(Model<?> model, Expression expr, boolean qual, MinMax minMax, BitSet statesOfInterest) throws PrismException
+	{
+		// Build product of Markov chain and DA for the LTL formula, and do any required exports
+		LTLModelChecker mcLtl = new LTLModelChecker(this);
+		AcceptanceType[] allowedAcceptance = {
+				AcceptanceType.RABIN,
+				AcceptanceType.REACH,
+				AcceptanceType.BUCHI,
+				AcceptanceType.STREETT,
+				AcceptanceType.GENERIC
+		};
+		LTLModelChecker.LTLProduct<IDTMC<Double>> product = mcLtl.constructDAProductForLTLFormula(this, (IDTMC<Double>) model, expr, statesOfInterest, allowedAcceptance);
+		doProductExports(product);
+
+		// Find accepting states + compute reachability probabilities
+		BitSet acc;
+		if (product.getAcceptance() instanceof AcceptanceReach) {
+			mainLog.println("\nSkipping BSCC computation since acceptance is defined via goal states...");
+			acc = ((AcceptanceReach)product.getAcceptance()).getGoalStates();
+		} else {
+			mainLog.println("\nFinding accepting BSCCs...");
+			acc = mcLtl.findAcceptingBSCCs(product.getProductModel(), product.getAcceptance());
+		}
+		mainLog.println("\nComputing reachability probabilities...");
+		IDTMCModelChecker mcProduct = new IDTMCModelChecker(this);
+		mcProduct.inheritSettings(this);
+		ModelCheckerResult res = mcProduct.computeReachProbs(product.getProductModel(), acc, minMax);
+		StateValues probsProduct = StateValues.createFromArrayResult(res, product.getProductModel());
+
+		// Output vector over product, if required
+		if (getExportProductVector()) {
+			mainLog.println("\nExporting product solution vector matrix to file \"" + getExportProductVectorFilename() + "\"...");
+			PrismFileLog out = new PrismFileLog(getExportProductVectorFilename());
+			probsProduct.print(out, false, false, false, false);
+			out.close();
+		}
+
+		// Mapping probabilities in the original model
+		StateValues probs = product.projectToOriginalModel(probsProduct);
+		probsProduct.clear();
+
+		return probs;
+	}
+
 	@Override
 	@SuppressWarnings("unchecked")
 	protected StateValues checkProbPathFormulaCosafeLTL(Model<?> model, Expression expr, boolean qual, MinMax minMax, BitSet statesOfInterest) throws PrismException
